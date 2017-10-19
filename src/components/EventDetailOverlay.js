8dfe7a15e1bb5593d1c709bdb8a43a19a88b426b
novyQ <novaqiu@gmail.com>
2017-10-18 20:49:23 -0700

    completed todos except ARIA attributes to overlay

--- src/components/EventDetailOverlay.js
+++ src/components/EventDetailOverlay.js
@@ -10,6 +10,35 @@ export default class EventDetailOverlay extends PureComponent {
         onClose: PropTypes.func.isRequired
     }
 
+    constructor() {
+        super();
+        this.handleOutsideClick = this.handleOutsideClick.bind(this);
+    }
+
+    handleOutsideClick(e) {
+        if (this.node.contains(e.target)) {
+            return;
+        }
+
+        this.props.onClose();
+    }
+
+    componentDidMount() {
+        document.body.classList.add('no-scroll');
+
+        document.addEventListener('keyup', (e) => {
+            if (e.keyCode === 27) this.props.onClose();
+        });
+
+        document.addEventListener('click', this.handleOutsideClick);
+    }
+
+    componentWillUnmount() {
+        document.body.classList.remove('no-scroll');
+
+        document.removeEventListener('click', this.handleOutsideClick);
+    }
+
     render() {
         let {event, onClose} = this.props;
         let {title, description, start, color, hours} = event;
@@ -29,7 +58,7 @@ export default class EventDetailOverlay extends PureComponent {
         // TODO: Support clicking outside of the overlay to close it
         // TODO: Support clicking ESC to close it
         return (
-            <section className="event-detail-overlay">
+            <section className="event-detail-overlay" ref={ node => {this.node = node}}>
                 <div className="event-detail-overlay__container">
                     <button
                         className="event-detail-overlay__close"
@@ -39,7 +68,7 @@ export default class EventDetailOverlay extends PureComponent {
                     <div>
                         {displayDateTime}
                         <span
-                            className="event-detail-overlay__color"
+                            className={`event-detail-overlay__color time-slot-event--${color}`}
                             title={`Event label color: ${color}`}
                         />
                     </div>
--- src/components/TimeSlotEvent.js
+++ src/components/TimeSlotEvent.js
@@ -1,25 +1,35 @@
 import React, {PureComponent, PropTypes} from 'react';
 import {EVENT_PROP_TYPE} from './constants';
-
+import moment from 'moment';
 import './TimeSlotEvent.css';
 
 export default class TimeSlotEvent extends PureComponent {
     static propTypes = {
         event: EVENT_PROP_TYPE.isRequired,
-        onSelect: PropTypes.func.isRequired,
+        onSelect: PropTypes.func.isRequired
     }
 
     render() {
         let {
-            event: {title, color},
+            event: {title, color, start},
             onSelect,
         } = this.props;
+
+        function addFadeClass(){
+            var eventTime = moment(start);
+            var now = moment();
+            if (now > eventTime) {
+                return "time-slot-event-fade";
+            } else {
+                return "";
+            }
+        }
 
         // TODO: Need a way to determine that the event is in the past so that it
         // can be displayed faded-out
 
         return (
-            <button className={`time-slot-event time-slot-event--${color}`} onClick={onSelect}>
+            <button className={`time-slot-event time-slot-event--${color} ${addFadeClass()}`} onClick={onSelect}>
                 {title}
             </button>
         );
